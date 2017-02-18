7018d1d3a983bb95f147b3cfc8fd695dad3e9b4b
tesseradecades <nathanjr95@gmail.com>
2017-02-18 04:30:56 -0500

    cracked caesar cipher, next add argument for top x guesses

--- caesarCipher.rb
+++ caesarCipher.rb
@@ -17,44 +17,44 @@ def codeMessage(c, rot, message)
 end
 
 def decode(rot, ch)
-	o = ch.ord-rot.to_i
-	if (123 > ch.ord and ch.ord > 96 and o < 96) or (64 < ch.ord and ch.ord < 91 and o < 64)
+	o = ch.downcase.ord-rot.to_i
+	if (o < 97)
 		o+=26
+	elsif (o > 122)
+		o-=26
 	end
-	return o.chr.to_s
+	return o.chr.to_s.upcase
 end
 
 def encode(rot, ch)
-	o = ch.ord+rot.to_i
-	if (123 > ch.ord and ch.ord > 96 and o > 123) or (64 < ch.ord and ch.ord < 91 and o > 91)
+	o = ch.downcase.ord+rot.to_i
+	if (o < 97)
+		o+=26
+	elsif (o > 122)
 		o-=26
 	end
-	return o.chr.to_s
+	return o.chr.to_s.upcase
 end
 
-#CRACKING THE CAESAR CIPHER w/ least square coming soon
+#CRACKING THE CAESAR CIPHER w/ least square
 def crack(message)
 	realFreq = Array[8.1, 1.5, 2.8, 4.3, 13.0, 2.2, 2.0, 6.1, 7.0, 0.15, 0.77, 7.0, 2.4,6.8, 7.5, 1.9, 0.095, 6.0, 6.3, 9.1, 2.8, 0.98, 2.4, 0.15, 2.0, 0.074]
 	#Step 1 Calculate letter frequencies of encrypted message
 	pseudoFreq = calculateLetterFrequencies(message)
 
-	#Step 2: check difference with ideal letter frequency distribution
-	diff = checkDiff(realFreq, pseudoFreq)
-	#Step 3: rotate pseudoFreq by 1 position
-
-	#puts("PSEUDOFREQ:\t"+pseudoFreq.length.to_s)
-	#puts(pseudoFreq)
-	#puts("END")
-	rotatedFreq = rotateFreq(pseudoFreq)
-	#puts("ROTATEDFREQ:\t"+rotatedFreq.length.to_s)
-	#puts(rotatedFreq)
-	#puts("END")
-
-	#Step 4: repeat steps 2 and 3 25times
-	diffArray = Array.new(1, diff) + repeat2And3(realFreq, rotatedFreq)
+	diffArray = Array.new
+	rotated = Array.new
+	rotated.replace(pseudoFreq)
+	for i in (0..realFreq.length) do
+		#Step 2: check difference with ideal letter frequency distribution
+		diffArray << checkDiff(realFreq, rotated)
+		#Step 3: rotate pseudoFreq by 1 position
+		rotated = rotated.rotate
+	end
 	#Step 5: find the array with the smallest difference
 	rot = diffArray.index(diffArray.min)
-	return rot #codeMessage("decode", rot, message)
+	#puts(diffArray)
+	return codeMessage("decode", rot, message)
 end
 
 def calculateLetterFrequencies(message)
@@ -88,33 +88,11 @@ def checkDiff(real, pseudo)
 		diffSquared << (diffs[x]**2)
 	end
 	#Step 3: return the average of the differences
-	return getAverage(diffSquared)
+	return rms(diffSquared)
 end
 
-def getAverage(vals)
-	s = 0.0
-	for i in 0..(vals.length-1)
-		s+=vals[i]
-	end
-	return (s / vals.length)
-end
-
-def rotateFreq(freq)
-	rotated = Array.new
-	rotated.replace(freq)
-	rotated = rotated.pop(1)+rotated
-	return rotated
-end
-
-def repeat2And3(real, rotated)
-	diffArray = Array.new
-	rot = rotated
-	for i in 0..24
-		diff = checkDiff(real, rot)
-		diffArray << diff
-		rot = rotateFreq(rot)
-	end
-	return diffArray
+def rms(seq)
+  return Math.sqrt(seq.inject(0.0) {|sum, x| sum + x*x} / seq.length)
 end
 
 
@@ -122,11 +100,11 @@ def main()
 	if ARGV.length > 1
 		command = ARGV[0]
 		if command == "crack"
-			#messages = ARGV[1..ARGV.length-1]
-			#messages.each do |m|
-			#	puts(crack(m))
-			#end
-			puts("Cracking Caesar Cipher coming soon!")
+			messages = ARGV[1..ARGV.length-1]
+			messages.each do |m|
+				puts(crack(m))
+			end
+			#puts("Cracking Caesar Cipher coming soon!")
 		elsif (command == "encode" or command == "decode") and ARGV.length > 2
 			rot = ARGV[1]
 			mess = ARGV[2..ARGV.length-1]
